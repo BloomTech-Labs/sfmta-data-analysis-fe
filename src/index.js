7915419ea69fb4a5987d7c4dd97f8ed7be1da978
mlazzell17@gmail.com <MicheSi>
2020-03-23 11:13:51 -0700

    added store, router, navbar and other components Co-authored-by: KonstadinosAngelis <angeliskonstadinos@gmail.com>
    Co-authored-by: qb1968 <mark.allison75@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import App from './App';
+import {rootReducer} from './reducers';
 
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
