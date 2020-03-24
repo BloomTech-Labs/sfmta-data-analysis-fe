01b9f726c29cc69934cc2acce19c1eacaa257289
mlazzell17@gmail.com <MicheSi>
2020-03-24 10:54:37 -0700

    added images of team, created profiles folder and files for each profile

---
+++ src/profiles/erik.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import erik from '../images/erik.jfif';
+
+const Erik = props => {
+    return (
+        <div>
+            <img src={erik}/>
+        </div>
+    )
+}
+
+export default Erik;
