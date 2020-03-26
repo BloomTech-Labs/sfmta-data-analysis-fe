c34d98e363db4c1ebd73337b982787ec266ccff2
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-03-26 11:45:46 -0700

    Continued work on the data map, and worked on getting the about us profiles to load properly Co-authored-by: MicheSi <mlazzell17@gmail.com> Co-authored-by: qb1968 <mark.allison75@gmail.com>

--- src/App.js
+++ src/App.js
@@ -8,18 +8,37 @@ import NavBar from './components/navBar';
 import LandingPage from './components/landingPage';
 import DataPage from './components/dataPage';
 import AboutUs from './components/aboutUs';
-import Connor from './profiles/connor';
 
+//profile imports
+import Connor from './profiles/connor';
+import Agustin from './profiles/agustin';
+import Cody from './profiles/cody';
+import Daniel from './profiles/daniel';
+import Erik from './profiles/erik';
+import Jonathan from './profiles/jonathan';
+import Jordan from './profiles/jordan';
+import Justin from './profiles/justin';
+import Mathias from './profiles/mathias';
+import Michelle from './profiles/michelle';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <NavBar />
-      <Route exact path="/" component={LandingPage} />
-      <Route path = "/data" component={DataPage} />
-      <Route path='/aboutus' component={AboutUs} />
-      <Route path='/profile' component={Connor} />
+        <NavBar />
+        <Route exact path="/" component={LandingPage} />
+        <Route path = "/data" component={DataPage} />
+        <Route path='/aboutus' component={AboutUs} />
+        <Route path='/agustin' component={Agustin}/>
+        <Route path='/cody' component={Cody} />
+        <Route path='/connor' component={Connor} />
+        <Route path='/daniel' component={Daniel} />
+        <Route path='/erik' component={Erik} />
+        <Route path='/jonathan' component={Jonathan} />
+        <Route path='/jordan' component={Jordan} />
+        <Route path='/justin' component={Justin} />
+        <Route path='/mathias' component={Mathias} />
+        <Route path='/michelle' component={Michelle} />
       </Router>
     </div>
   );
--- src/profiles/michelle.js
+++ src/profiles/michelle.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
 import michelle from '../images/michelle.jfif';
 
 const Michelle = props => {
     return (
-        <div>
-            <img src={michelle}/>
+        <div className='profileDiv'>
+            <Card>
+                <CardImg top width="100%" src={michelle} alt="Profile picture" />
+                <CardBody>
+                    <CardTitle>Michelle Sirimanivong</CardTitle>
+                    <CardSubtitle>Web Developer</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>View Github</Button>
+                </CardBody>
+            </Card>
         </div>
     )
 }
