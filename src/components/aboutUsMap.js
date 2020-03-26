e111cd9154e993c601b1ce170a15d4b04b970b96
Michelle Sirimanivong <55412009+MicheSi@users.noreply.github.com>
2020-03-26 16:27:51 -0700

    Revert "Profile cards about us"

--- src/components/aboutUsMap.js
+++ src/components/aboutUsMap.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom'
-import { toggleModal } from '../actions/';
+import React from 'react';
 
 import createPlotlyComponent from "react-plotly.js/factory";
 
@@ -12,52 +9,31 @@ Plotly.register([
 
 const Plot = createPlotlyComponent(Plotly);
 
-const AboutUsMap = props => {
-    console.log('props', props)
-useEffect(() => {
-    props.toggleModal();
-}, [])
-    //Handle click to render component based on which team member was clicked
-    const handleClick = (element) => {
-        return(
-        (element.points[0].hovertext === 'Agustin Vargas') ? props.history.push('/agustin'):
-        (element.points[0].hovertext === 'Cody Holman') ? props.history.push('/cody'):
-        (element.points[0].hovertext === 'Connor Angelis') ? props.toggleModal(): 
-        (element.points[0].hovertext === 'Daniel Aguilar') ? props.history.push('/daniel'):
-        (element.points[0].hovertext === 'Erik Sandoval') ? props.history.push('/erik'):
-        (element.points[0].hovertext === 'Jonathan Allison') ? props.history.push('/jonathan'):
-        (element.points[0].hovertext === 'Jordan Ireland') ? props.history.push('/jordan'):
-        (element.points[0].hovertext === 'Justin Menendez') ? props.history.push('/justin'):
-        (element.points[0].hovertext === 'Mathias Skerden') ? props.history.push('/mathias'):
-        (element.points[0].hovertext === 'Michelle Sirimanivong') ? props.history.push('/michelle'):
-        (element.points[0].hovertext === 'Agustin Vargas') ? props.history.push('/agustin') : null
-    )}
 
+const AboutUsMap = props => {
+    require('dotenv').config()
+    console.log(process.env.REACT_APP_PLOTLY_API_KEY)
     return (
-        <div>
         <Plot
-        data={[{
-            "lat": [33.633322, 36.12870, 45.443604, 32.624580, 33.748975, 33.557465, 30.458172, 34.0522, 33.895847, 58.410460],
-            "lon": [-112.457310, -79.40860, -122.840082, -85.449071, -116.958139, -111.899536, -84.216021, -118.2437, -118.220070, 8.726921],
-            "hovertext": ["Michelle Sirimanivong", "Jonathan Allison", "Connor Angelis", "Cody Holman", 'Erik Sandoval', 'Jordan Ireland', 'Justin Menendez', 'Agustin Vargas', 'Daniel Aguilar', 'Mathias Skerden'],
-            "mode": "markers",
-            "type": "scattermapbox",
-            "marker": {"size": 12},
-        }]}
-        layout={{"height": 750, "width": 1000, "mapbox": {"accesstoken": process.env.REACT_APP_PLOTLY_API_KEY, "style": "dark", "zoom": 3.5, "center": {"lat": 37.945318626818434, "lon": -98.85080973073082} }, "margin": { "b": 0, "l": 0, "r": 0, "t": 0 }, "showlegend": false, "dragmode": "lasso"}}
-        config={{
-            "displayModeBar": false,
-            "responsive": true,
-            "scrollZoom": false,
-        }}
-        onClick={(data) => {handleClick(data)}}
+            data={[{
+                "lat": [33.633322, 36.12870, 45.443604, 32.624580],
+                "lon": [-112.457310, -79.40860, -122.840082, -85.449071],
+                "hovertext": ["test1", "test2", "test3", "Cody Holman"],
+                "mode": "markers",
+                "type": "scattermapbox",
+                "marker": {"size": 12},
+            }]}
+            layout={{"height": 1000, "width": 1000, "mapbox": {"accesstoken": process.env.REACT_APP_PLOTLY_API_KEY, "style": "dark", "zoom": 3.2009500376323845, "center": {"lat": 37.945318626818434, "lon": -94.85080973073082} }, "margin": { "b": 0, "l": 0, "r": 0, "t": 0 }, "showlegend": false, "xaxis": {"fixedrange": true}, "yaxis": {"fixedrange": true}}}
+            config={{
+                "displayModeBar": false,
+                "responsive": true,
+                "scrollZoom": false,
+                "dragmode": false
+            }}
         />
-        </div>
 )}
 
-const mapStateToProps = state => {
-    return {
-        modal: state.modal
-    }
-}
-export default connect(mapStateToProps, {toggleModal})(AboutUsMap);
+export default AboutUsMap;
+
+    
+
--- src/components/dataPage.js
+++ src/components/dataPage.js
@@ -1,138 +1,57 @@
-import React, { useState, useEffect } from "react";
-import createPlotlyComponent from "react-plotly.js/factory";
+import React from 'react';
+// import TestMap from './testMap';
+
+// const DataPage = props => {
+//     return (
+//         <div>
+//             Data Page with Bunches & Gaps
+//             <div>
+//                 <TestMap/>
+//                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique nulla aliquet enim tortor at. Sit amet mauris commodo quis imperdiet massa tincidunt nunc. Magna sit amet purus gravida. Ultrices in iaculis nunc sed augue lacus. Consequat semper viverra nam libero justo laoreet sit amet cursus. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Adipiscing elit ut aliquam purus sit. Sit amet venenatis urna cursus eget nunc scelerisque viverra mauris. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Pulvinar proin gravida hendrerit lectus. Odio aenean sed adipiscing diam. Tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus. Magna etiam tempor orci eu lobortis elementum. Nec tincidunt praesent semper feugiat.</p>
+//             </div>
+//             <div>
+//                 <TestMap/>
+//                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique nulla aliquet enim tortor at. Sit amet mauris commodo quis imperdiet massa tincidunt nunc. Magna sit amet purus gravida. Ultrices in iaculis nunc sed augue lacus. Consequat semper viverra nam libero justo laoreet sit amet cursus. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Adipiscing elit ut aliquam purus sit. Sit amet venenatis urna cursus eget nunc scelerisque viverra mauris. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Pulvinar proin gravida hendrerit lectus. Odio aenean sed adipiscing diam. Tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus. Magna etiam tempor orci eu lobortis elementum. Nec tincidunt praesent semper feugiat.</p>
+//             </div>
+//         </div>
+//     )
+// }
 
 import { connect } from "react-redux";
-import { fetchRoutes, fetchLayouts, fetchNames } from "../actions/index";
-
+import { fetchRoutes } from "../actions/index";
 import Loading from "./Loading";
-import { Input, Form } from "reactstrap";
 
-//Importing Plot.ly react
-var Plotly = require("plotly.js/lib/core");
-Plotly.register([require("plotly.js/lib/scattermapbox")]);
-const Plot = createPlotlyComponent(Plotly);
-
-//Component
 function RouteList(props) {
-  //Get route data
-  useEffect(() => {
-    props.fetchRoutes();
-    props.fetchLayouts();
-    props.fetchNames();
-  }, []);
-
-  //Setup state for selecting/submitting the route data
-  const [selectedRoute, setSelectedRoute] = useState(0);
-  const [routeData, setRouteData] = useState({
-    lat: [],
-    lon: [],
-    marker: {"color": "blue"},
-    mode: "lines",
-    type: "scattermapbox"
-  });
-  const [stopData, setStopData] = useState({
-    lat: [],
-    long: [],
-    marker: {},
-    mode: "",
-    type: ""
-  });
-
-  const handleRouteSelect = e => {
-    setSelectedRoute(e.target.value);
-  };
-
-  //Displaying the route that is selected
-  const handleRouteSubmit = e => {
-    e.preventDefault();
-
-    let traces = []
-
-    props.names.map(route => {
-      //Finding the selected route
-      if (route.route_name === selectedRoute) {
-        route.traces.map(trace => {
-          //Check if it's stop data, and set state if it is
-          if (props.allroutes[trace].mode === "markers") {
-             setStopData({
-              ...stopData,
-              lat: props.allroutes[trace].lat,
-              long: props.allroutes[trace].lon,
-              marker: props.allroutes[trace].marker,
-              mode: props.allroutes[trace].mode,
-              type: props.allroutes[trace].type
-            });
-          }
-          //Take each trace object and add it to the traces array
-          return traces.push(props.allroutes[trace])
-        });
-      }
-    });
-
-    //Add the stops state to the end of the traces
-    traces.push(stopData)
-
-    //Set route data state to the traces array which is get's displayed on the map
-    setRouteData(traces)
-  };
-
-  //Grabbing plotly API key
-  require("dotenv").config();
-  console.log(routeData)
-  return (
-    <div>
-      <div>
+    console.log("RouteList", props);
+    return (
+      <>
+      
+        <button onClick={props.fetchRoutes}>Get Data</button>
+    {props.isFetching && <div><Loading/></div>}
         {props.error && <div>{props.error.message}</div>}
-        {props.isFetching && (<div><Loading /></div>)}
-
-        <Form onSubmit={handleRouteSubmit}>
-          <Input
-            type="select"
-            value={selectedRoute}
-            onChange={handleRouteSelect}
-            name="selectedRoute"
-          >
-            <option>Select a Route</option>
-            {props.names.map(name => (
-              <option value={name.route_name} name="selectedRoute">
-                {name.route_name}
-              </option>
-            ))}
-          </Input>
-          <button>Get Data</button>
-        </Form>
-        <Plot
-          data={routeData}
-          layout={{
-            height: 700,
-            mapbox: {
-              accesstoken: process.env.REACT_APP_PLOTLY_API_KEY,
-              style: "outdoors",
-              zoom: 11.25,
-              center: { lat: 37.76, lon: -122.4 }
-            },
-            margin: { b: 0, l: 0, r: 0, t: 0 },
-            showlegend: false,
-            width: 1000
-          }}
-        />
-      </div>
-    </div>
-  );
-}
-
-const mapStateToProps = state => {
-  return {
+        <div>
+          {props.allroutes.map(p => (
+            
+            <p className="allroutes" key={p.name}>
+              {p.name}
+              
+            </p>
+            
+          ))}
+        </div>
+      </>
+    );
+  }
+  
+  const mapStateToProps = state => {
+    return{
     allroutes: state.allroutes,
-    layout: state.layout,
-    names: state.names,
     error: state.error,
-    isFetching: state.isFetching
+    isFetching: state.isFetching 
+    }
   };
-};
+  
+  export default connect( 
+    mapStateToProps, {fetchRoutes}
+  )(RouteList);
 
-export default connect(mapStateToProps, {
-  fetchRoutes,
-  fetchLayouts,
-  fetchNames
-})(RouteList);
