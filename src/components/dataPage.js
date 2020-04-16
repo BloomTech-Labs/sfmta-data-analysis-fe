a104746e33c638d27506d99298daeb2c46a25c14
mlazzell17@gmail.com <MicheSi>
2020-04-16 10:37:31 -0700

    condensed lines of code to fix code climate issues Co-authored-by: qb1968 <mark.allison75@gmail.com> Co-authored-by: KonstadinosAngelis <angeliskonstadinos@gmail.com> Co-authored-by: cholman <holman.cody@gmail.com>

--- src/components/dataPage.js
+++ src/components/dataPage.js
@@ -21,11 +21,8 @@ const RouteList = (props) => {
   const [selectedType, setSelectedType] = useState("");
 
   //State for the list of routes that get displayed when type is selected
-  const [routesBasedOnType, setRoutesBasedOnType] = useState({
-    typeNames: [],
-    typeIds: []
-  });
-
+  const [routesBasedOnType, setRoutesBasedOnType] = useState({typeNames: [], typeIds: []});
+    
   //State for the specific route that get's selected
   const [selectedRoute, setSelectedRoute] = useState("");
   
@@ -50,9 +47,7 @@ const RouteList = (props) => {
   const [typeAndRouteData, setTypeAndRouteData] = useState({})
   useEffect(() => {
     axios.get('https://sfmta-test.herokuapp.com/routes-info')
-    .then(res => {
-      setTypeAndRouteData(res.data)
-    })
+    .then(res => {setTypeAndRouteData(res.data)})
     .catch(err => {console.log(err)})
   }, []);
   
@@ -73,18 +68,12 @@ const RouteList = (props) => {
     //Set the state of the routes list to the correct array based on type selection
     typeAndRouteData.type.find((name, index) => {
       if(name === e.target.value){
-        setRoutesBasedOnType({
-        typeNames: typeAndRouteData.name[index],
-        typeIds: typeAndRouteData.id[index]
-      })
+        setRoutesBasedOnType({typeNames: typeAndRouteData.name[index], typeIds: typeAndRouteData.id[index]})
      }
    })
   };
   
-  const [inputValidationState, setInputValidationState] = useState({
-    typeValidation: false,
-    routeValidation: false
-  })
+  const [inputValidationState, setInputValidationState] = useState({typeValidation: false, routeValidation: false})
 
   //Displaying the route that is selected on the Plotly
   const handleRouteSubmit = e => {
@@ -93,51 +82,29 @@ const RouteList = (props) => {
 
     //Simple validation for type/route. In order: (Neither selected, only type selected, only route selected, both selected) <-- This is to reset state if everything passes
     if(!selectedType && !selectedRoute){
-      console.log('Selected Neither')
-      setInputValidationState({
-        typeValidation: true,
-        routeValidation: true
-      })
+      setInputValidationState({typeValidation: true, routeValidation: true})
       return 
     }
 
     if(!selectedType){
-      console.log('didnt select type')
-    setInputValidationState({
-      typeValidation: true,
-      routeValidation: false
-      })
+    setInputValidationState({typeValidation: true, routeValidation: false})
       return 
     }
 
     if(!selectedRoute){ 
-      console.log('didnt select route')
-      setInputValidationState({
-      typeValidation: false,
-      routeValidation: true
-      })
+      setInputValidationState({typeValidation: false, routeValidation: true})
       return 
     }
 
     //Reset Validation
-    setInputValidationState({
-      typeValidation: false,
-      routeValidation: false
-    })
-
+    setInputValidationState({typeValidation: false,routeValidation: false})
+      
     //Finding the selected route
       if(lineData.traces === undefined){return}
       lineData.traces.map(trace => {
         //Check if it's stop data, and set state if it is
         if (trace.mode === "markers") {
-          setStopData({
-            ...stopData,
-            lat: trace.lat,
-            long: trace.lon,
-            marker: trace.marker,
-            mode: trace.mode,
-            type: trace.type
-          });
+          setStopData({...stopData, lat: trace.lat, long: trace.lon, marker: trace.marker, mode: trace.mode, type: trace.type});
         }
         //Take each trace object and add it to the traces array
         return traces.push(trace)
@@ -149,39 +116,19 @@ const RouteList = (props) => {
     //Set route data state to the traces array which is get's displayed on the map
     setRouteData(traces) 
   };
-  
-  //Grabbing plotly API key
-  require("dotenv").config();
 
   return (
     <div>
-      {props.isFetching ? (
-        <Loading />
-      ) : (
-      <div>
-        {props.error && <div>{props.error.message}</div>}
+      {props.isFetching ? (<Loading />) : (<div>{props.error && <div>{props.error.message}</div>}
 
         <Form onSubmit={handleRouteSubmit}>
-          <Input
-            type="select"
-            onChange={handleTypeSelect}
-            name="selectedType"
-            value={selectedType}
-          >
+          <Input type="select" onChange={handleTypeSelect} name="selectedType" value={selectedType}>
             <option name="selectedType">Select a type</option>
-            {typeAndRouteData.type && 
-            typeAndRouteData.type.map(typeName => (
-              <option key={typeName}>{typeName}</option>))
-            }
+            {typeAndRouteData.type && typeAndRouteData.type.map(typeName => (<option key={typeName}>{typeName}</option>))}
           </Input>
             {inputValidationState.typeValidation && <div style={{color: "red"}}>Please Enter a Type</div>}
 
-          <Input
-            type="select"
-            value={selectedRoute}
-            onChange={handleRouteSelect}
-            name="selectedRoute"
-          >
+          <Input type="select" value={selectedRoute} onChange={handleRouteSelect} name="selectedRoute">
           {routesBasedOnType.typeNames.length === 0 && <option>Select type to see routes</option>}
           {routesBasedOnType.typeNames.length > 0 && <option>Select a route!</option>}
             { routesBasedOnType.typeNames.length > 0 && 
@@ -197,12 +144,7 @@ const RouteList = (props) => {
           data={routeData}
           layout={{
             height: 600,
-            mapbox: {
-              accesstoken: process.env.REACT_APP_PLOTLY_API_KEY,
-              style: "dark",
-              center: { lat: 37.748, lon: -122.4 },
-              zoom: 11.25
-            },
+            mapbox: {accesstoken: process.env.REACT_APP_PLOTLY_API_KEY, style: "dark", center: { lat: 37.748, lon: -122.4 }, zoom: 11.25},
             margin: { b: 0, l: 0, r: 0, t: 0 },
             showlegend: false,
             width: 1000
