0074dca1a9448de031304f5ca99697268a113862
Kevin <knphillips01@gmail.com>
2020-05-18 13:34:44 -0500

    updated About Us to include Labs24 - still need to plot on map

--- src/components/profileData.jsx
+++ src/components/profileData.jsx
@@ -1,4 +1,14 @@
+import Kevin from '../images/Kevin.jpg';
+import Nick from '../images/Nick.jpg';
+import Bairon from '../images/Bairon.jpg';
+import JustinR from '../images/JustinR.jpg';
+import AustinR from '../images/AustinR.jpg';
 import agustin from '../images/agustin.jpg';
+import JustinK from '../images/JustinK.jpg';
+import Isaac from '../images/Isaac.jpg';
+import Jonathan from '../images/Jonathan.png';
+import Ramses from '../images/Ramses.jpg';
+import Mary from '../images/Ramses.jpg';
 import cody from '../images/cody.jpg';
 import connor from '../images/connor.jpg';
 import erik from '../images/erik.jpg';
@@ -11,15 +21,96 @@ import michelle from '../images/michelle.jpg';
 const profileData = [
     {
         id: 1,
-        name: 'Agustin Vargas',
-        title: 'Data Scientist',
-        bio: 'An aspiring data scientist, with a background in scientific research and higher education administration; intuitive by nature and optimistic by habit.  My role in this project was to work with the DS team to gain insight into the traffic patterns of vehicles within the San Francisco public transit system.',
-        img: agustin,
-        github: 'https://github.com/avdata',
-        linkedIn: 'http://linkedin.com/in/vargasstem'
+        name: 'Kevin Phillips',
+        title: 'Full Stack Web Developer',
+        bio: 'bio here',
+        img: Kevin,
+        github: 'https://github.com/kphillips001/',
+        linkedIn: 'https://www.linkedin.com/in/kevin-n-phillips/'
     },
     {
         id: 2,
+        name: 'Nick Migel',
+        title: 'Full Stack Web Developer',
+        bio: 'bio here',
+        img: Nick,
+        github: 'https://github.com/rivercrow25',
+        linkedIn: 'https://www.linkedin.com/in/nick-migel-67b4831a2/'
+    },
+    {
+        id: 3,
+        name: 'Bairon Paz',
+        title: 'Full Stack Web Developer',
+        bio: 'bio here',
+        img: Bairon,
+        github: 'https://github.com/MrR3set',
+        linkedIn: 'https://www.linkedin.com/in/byronpaz'
+    },
+    {
+        id: 4,
+        name: 'Justin Russell',
+        title: 'Full Stack Web Developer',
+        bio: 'bio here',
+        img: JustinR,
+        github: 'https://github.com/justinruss24',
+        linkedIn: 'https://www.linkedin.com/in/justinrussell-web/'
+    },
+    {
+        id: 5,
+        name: 'Justin Kuenzinger',
+        title: 'Full Stack Web Developer',
+        bio: 'bio here',
+        img: JustinK,
+        github: 'https://github.com/jkuenzinger',
+        linkedIn: 'https://www.linkedin.com/in/justin-kuenzinger-2a8a2b1a0/'
+    },
+    {
+        id: 6,
+        name: 'Isaac Grove',
+        title: 'Data Scientist',
+        bio: 'bio here',
+        img: Isaac,
+        github: 'https://github.com/isaacgrove',
+        linkedIn: 'https://www.linkedin.com/in/isaac-grove-01300b177/'
+    },
+    {
+        id: 7,
+        name: 'Jonathan Duke',
+        title: 'Data Scientist',
+        bio: 'bio here',
+        img: Jonathan,
+        github: 'https://github.com/jonDuke',
+        linkedIn: 'https://www.linkedin.com/in/jonduke90/'
+    },
+     {
+         id: 8,
+         name: 'Austin Robinson',
+         title: 'Data Scientist',
+         bio: 'bio here',
+         img: AustinR,
+         github: 'https://github.com/austiezr',
+         linkedIn: 'https://www.linkedin.com/in/austin-ziech-robinson/'
+    },
+    {
+        id: 9,
+        name: 'Ramses Gasque',
+        title: 'Data Scientist',
+        bio: 'bio here',
+        img: Ramses,
+        github: '',
+        linkedIn: ''
+    },
+    {
+        id: 10,
+        name: 'Mary Streletska',
+        title: 'UX Designer',
+        bio: 'bio here',
+        img: Mary,
+        github: '',
+        linkedIn: ''
+    },
+    {
+        id: 11,
         name: 'Cody Holman',
         title: 'Web Developer',
         bio: 'Hobbyist tech nerd turned pro. A full stack web developer with a never ending desire to improve.',
@@ -28,7 +119,7 @@ const profileData = [
         linkedIn: 'https://www.linkedin.com/in/cody-holman'
     },
     {
-        id: 3,
+        id: 12,
         name: 'Connor Angelis',
         title: 'Web Developer',
         bio: 'A growing front-end developer, with a short lifetime of always working with computers and technology. A nerd at heart and constantly working to dig myself a home in the software industry. My role in this project was working with the Plotly React API to bring a visualization of the San Francisco metro system to life.',
@@ -37,7 +128,7 @@ const profileData = [
         linkedIn: 'https://www.linkedin.com/in/konstadinos-a-a8a641199/'
     },
     {
-        id: 4,
+        id: 13,
         name: 'Erik Sandoval',
         title: 'Web Developer & Team Lead',
         bio: 'Nintendo and Hardware enthusiast, specializes in making and fixing own mistakes.',
@@ -46,7 +137,7 @@ const profileData = [
         linkedIn: ''
     },
     {
-        id: 5,
+        id: 14,
         name: 'Jonathan Allison',
         title: 'Web Developer',
         bio: 'Jonathan is a Web Developer from North Carolina and proud husband, father, and grandfather. Not afraid to still chase my dreams.',
@@ -55,7 +146,7 @@ const profileData = [
         linkedIn: 'https://www.linkedin.com/in/jonathan-allison-1a4136196/'
     },
     {
-        id: 6,
+        id: 15,
         name: 'Jordan Ireland',
         title: 'Data Scientist',
         bio: 'Data Scientist by day and game developer by night. I have been programming for over 10 years and have a background in medicine and optics. I worked on creating the API that would be called to get the front end information and to support the rest of the Data Science team.',
@@ -64,7 +155,7 @@ const profileData = [
         linkedIn: 'https://www.linkedin.com/in/jordan-b-ireland/'
     },
     {
-        id: 7,
+        id: 16,
         name: 'Justin Menendez',
         title: 'UX Designer',
         bio: 'Like if Andre 3000 were a bohemian mystic.  Helped with Design and Creative Thinking.  Do the Good.',
@@ -73,7 +164,7 @@ const profileData = [
         linkedIn: 'https://www.linkedin.com/in/justinmenendez/'
     },
     {
-        id: 8,
+        id: 17,
         name: 'Mathias Skreden',
         title: 'Data Scientist',
         bio: 'Bio will go here.',
@@ -82,14 +173,23 @@ const profileData = [
         linkedIn: 'https://www.linkedin.com/in/skredenmathias/'
     },
     {
-        id: 9,
+        id: 18,
         name: 'Michelle Sirimanivong',
         title: 'Web Developer',
         bio: 'Michelle is a full-stack web developer from Arizona. In her free time, she enjoys baking, playing video games and spending time with her husband and her 3 dogs.',
         img: michelle,
         github: 'https://github.com/MicheSi',
         linkedIn: 'https://www.linkedin.com/in/michellesirimanivong/'
-    }
+    },
+    {
+        id: 19,
+        name: 'Agustin Vargas',
+        title: 'Data Scientist',
+        bio: 'An aspiring data scientist, with a background in scientific research and higher education administration; intuitive by nature and optimistic by habit.  My role in this project was to work with the DS team to gain insight into the traffic patterns of vehicles within the San Francisco public transit system.',
+        img: agustin,
+        github: 'https://github.com/avdata',
+        linkedIn: 'http://linkedin.com/in/vargasstem'
+    },
 ]
 
 export default profileData;
