e62131cc80839695447ce3ff43ad7cb68cb429a1
Kevin <knphillips01@gmail.com>
2020-05-27 16:04:39 -0500

    few changes to map, changed map to autosize

--- src/components/Dashboard/Map.jsx
+++ src/components/Dashboard/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import createPlotlyComponent from "react-plotly.js/factory";
 
 //Importing Plot.ly react
@@ -9,32 +9,36 @@ const Plot = createPlotlyComponent(Plotly);
 //Component
 const Map = () => {
 
-  //Centers the map on San Francisco
-  const [mapState, setMapState] = useState({
-    zoom: 11.25,
-    center: { lat: 37.742, lon: -122.438 },
-    bearing: 0,
-    pitch: 0
-  })
+//Centers the map on San Francisco
+const [mapState, setMapState] = useState({
+  zoom: 11,
+  center: { lat: 37.742, lon: -122.438 },
+  bearing: 0,
+  pitch: 0
+})
 
-  const [routeData, setRouteData] = useState([{
-    lat: [],
-    lon: [],
-    marker: { "color": "blue" },
-    mode: "lines",
-    type: "scattermapbox"
-  }]);
+const [zoom, setZoom] = useState([{
+  lat: [],
+  lon: [],
+  type: "scattermapbox"
+}]);
 
   return (
     <div>
       {/* the map being rendered on the website */}
         <Plot
-          data={routeData}
+          data={zoom}
           layout={{
-            height: 600, width: 1000,
-            mapbox: { accesstoken: process.env.REACT_APP_PLOTLY_API_KEY, style: "dark", center: mapState.center, zoom: mapState.zoom, bearing: mapState.bearing, pitch: mapState.pitch },
-            margin: { b: 0, l: 0, r: 0, t: 0 },
-            showlegend: false,
+            autosize: true,
+            //height: 600, width: 1000,
+            mapbox: 
+              { accesstoken: process.env.REACT_APP_PLOTLY_API_KEY, style: "dark", 
+              center: mapState.center, 
+              zoom: mapState.zoom, 
+              bearing: mapState.bearing, 
+              pitch: mapState.pitch },
+              margin: { b: 0, l: 0, r: 0, t: 0 },
+              showlegend: false,
           }}
           onRelayout={(e) => setMapState({
             zoom: e["mapbox.zoom"],
