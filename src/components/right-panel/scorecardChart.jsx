ac3078c0bd8b64066d33adf529edd77b4be12f60
MrR3set <bairon.pazrobles@gmail.com>
2020-06-02 13:39:40 -0700

    added text in the center

--- src/components/right-panel/scorecardChart.jsx
+++ src/components/right-panel/scorecardChart.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, defaults } from "react-chartjs-2";
 
 
 export const ScoreCardChart = () => {
+
+  
 
   return (
     <>
@@ -24,12 +26,31 @@ export const ScoreCardChart = () => {
 
         options = {{
             cutoutPercentage:65,
-            tooltips:true,
-            // title:{
-            //   display:true,
-            //   text:"80%",
-            //   position:"chartArea"
-            // }
+            tooltips:{
+              enabled:false
+            },
+        }}
+
+
+        plugins ={{
+          beforeDraw: function(chart) {
+            var width = chart.chart.width,
+                height = chart.chart.height,
+                ctx = chart.chart.ctx;
+            
+            ctx.restore();
+            var fontSize = (height / 114).toFixed(2);
+            ctx.font = fontSize + "em sans-serif";
+            ctx.textBaseline = "middle";
+            ctx.fillStyle = "white"
+        
+            var text = "75%",
+                textX = Math.round((width - ctx.measureText(text).width) / 2),
+                textY = height / 2;
+        
+            ctx.fillText(text, textX, textY);
+            ctx.save();
+          }
         }}
       />
     </>
