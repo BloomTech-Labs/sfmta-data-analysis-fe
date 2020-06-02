43b7570fcc87f82a432c255a62242a5a6a135f4e
MrR3set <bairon.pazrobles@gmail.com>
2020-06-02 13:43:43 -0700

    added text in the middle of the chart

--- src/components/right-panel/scorecardChart.jsx
+++ src/components/right-panel/scorecardChart.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Doughnut, defaults } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 
 export const ScoreCardChart = () => {
-<<<<<<< HEAD
 
   
 
-=======
->>>>>>> 39300f0a0d229aa96a020692f974842e027048dd
   return (
     <>
       <Doughnut
@@ -33,7 +30,6 @@ export const ScoreCardChart = () => {
             },
           ],
         }}
-<<<<<<< HEAD
 
         options = {{
             cutoutPercentage:65,
@@ -50,28 +46,18 @@ export const ScoreCardChart = () => {
                 ctx = chart.chart.ctx;
             
             ctx.restore();
-            var fontSize = (height / 114).toFixed(2);
+            var fontSize = (height / 90).toFixed(2);
             ctx.font = fontSize + "em sans-serif";
             ctx.textBaseline = "middle";
             ctx.fillStyle = "white"
         
-            var text = "75%",
+            var text = "80%",
                 textX = Math.round((width - ctx.measureText(text).width) / 2),
                 textY = height / 2;
         
             ctx.fillText(text, textX, textY);
             ctx.save();
           }
-=======
-        options={{
-          cutoutPercentage: 65,
-          tooltips: true,
-          // title:{
-          //   display:true,
-          //   text:"80%",
-          //   position:"chartArea"
-          // }
->>>>>>> 39300f0a0d229aa96a020692f974842e027048dd
         }}
       />
     </>
