36c317c22ec2dc0b5f3e01830480839467da7423
Kevin <knphillips01@gmail.com>
2020-06-03 14:58:32 -0500

    added labels to line-graph

--- src/components/line-chart/line-chart.jsx
+++ src/components/line-chart/line-chart.jsx
@@ -4,30 +4,32 @@ import { Line } from 'react-chartjs-2'
 export const LineChart = () => {
   return (
     <div className="line-chart">
-    <Line 
-      data={{
-        datasets: [
-          {
-            data: [70, 30, 20, 10, 85],
-            backgroundColor: ["#FBD03F", "#232323"],
-            borderColor: '#00FFFF',
-            fill: false
-          },
-          {
-            data: [50, 10, 90, 5, 65],
-            backgroundColor: ["#FBD03F", "#232323"],
-            borderColor: '#FF4500',
-            fill: false,
-          }
-        ]
-      }}
+      <Line 
+        data={{
+          datasets: [
+            {
+              data: [70, 30, 20, 10, 85],
+              label: '% gapped',
+              backgroundColor: ["#FBD03F", "#232323"],
+              borderColor: '#00FFFF',
+              fill: false
+            },
+            {
+              data: [50, 10, 90, 5, 65],
+              label: '# bunches',
+              backgroundColor: ["#FBD03F", "#232323"],
+              borderColor: '#FF4500',
+              fill: false,
+            }
+          ]
+        }}
 
-     options={{
-         responsive: true,
-         aspectRatio: 1,
-         maintainAspectRatio: false
-      }}
-    />
+      options={{
+          responsive: true,
+          aspectRatio: 1,
+          maintainAspectRatio: false
+        }}
+      />
     </div>
   )
 };
