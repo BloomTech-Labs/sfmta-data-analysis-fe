0bdc81a6ffa24f2f89b273f311ca9e74a4c0f5c8
nick migel <nickmigel2@gmail.com>
2020-06-08 17:49:52 -0400

    fixed merge conflicts

--- src/components/aboutUsMap.jsx
+++ src/components/aboutUsMap.jsx
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import React, { useState, useRef } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
 import * as devData from './data/developer-data.json'
 
 function AboutUs() {
-=======
-import React, { useState } from 'react';
-import ReactMapGL, {  Marker, Popup } from 'react-map-gl';
-import * as devData from './data/developer-data.json'
-
-function AboutUs() {
-
->>>>>>> 4868732c9706f9c08ac0fafef8982977a5657b88
   // setup map of US based on central coorindate points 
   const [viewport, setViewPort] = useState({
     latitude: 37.71,
@@ -19,13 +10,7 @@ function AboutUs() {
     width: '70vw',
     height: '65vh',
   });
-<<<<<<< HEAD
   const mapRef = useRef();
-=======
-    
-const [selectedDev, setSelectedDev] = useState(null);
-
->>>>>>> 4868732c9706f9c08ac0fafef8982977a5657b88
 
   return (
     <div className="aboutus-map">
@@ -52,73 +37,7 @@ const [selectedDev, setSelectedDev] = useState(null);
         ))}
       </ReactMapGL>
     </div>
-<<<<<<< HEAD
 
   )
 }
-=======
-
-    const [viewport, setViewPort] = useState({
-      latitude: 37.71,
-      longitude: -96.24,
-      width: '59.1vw',
-      height: '61vh',
-    });
-
-
-return (
-<div className="aboutus-map">
- <ReactMapGL
-   {...viewport}
-    //fixes the map to still position
-    minZoom={3.7}
-    maxZoom={4.1}
-    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-    mapStyle='mapbox://styles/sfmtalambda/ckasm81dc3oml1jlls8kbz5mc'
-    onViewportChange={viewport => {
-    setViewPort(viewport);
-     }}
-     
-    >
-    {devData.features.map(dev => (
-        //places marker on the map
-        <Marker 
-          key={dev.geometry.coordinates} 
-          latitude={dev.geometry.coordinates[1]}
-          longitude={dev.geometry.coordinates[0]}
-          >
-          <button 
-            className="marker-btn"
-            onClick={e => {
-              e.preventDefault();
-              setSelectedDev(dev)
-            }}
-            >
-            <i class="fa fa-map-marker" aria-hidden="true"></i>
-          </button>
-        </Marker>
-      ))}
-
-      {selectedDev ? (
-          <Popup
-            latitude={selectedDev.geometry.coordinates[1]}
-            longitude={selectedDev.geometry.coordinates[0]}
-            onClose={() => {
-              setSelectedDev(null);
-            }}
-          >
-            <div className="popup-text">
-              <h2>{selectedDev.properties.name}</h2> 
-              <h4>{selectedDev.properties.role}</h4>
-            </div>
-          </Popup>
-        ) : null} 
-
-    </ReactMapGL>
-</div>
-
-  );
-}
-
->>>>>>> 4868732c9706f9c08ac0fafef8982977a5657b88
 export default AboutUs
