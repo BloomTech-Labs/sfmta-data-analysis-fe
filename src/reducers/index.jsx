a4601859df73f0b418c072870f59bd1229ec2a83
Justin Russell <Justin@justins-macbook.lan>
2020-06-08 10:03:04 -0700

    more changes

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,24 +1,29 @@
-
-const initialState = {
-  type: {
-    features: [
-      {
-        type: '',
-        geometry: {
-          type: '',
-          coordinates: []
+import { SET_DATE, SET_TRANSIT_TYPE } from "../actions/index.jsx";
+  
+  const initialState = {
+    type: {
+      features: [
+        {
+          type: "",
+          geometry: {
+            type: "",
+            coordinates: [],
+          },
+        },
+      ],
+    },
+    isLoading: false,
+  };
+  
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case SET_TRANSIT_TYPE:
+        return {
+          isLoading: false,
         }
-      }
-    ]
+      default:
+        return state;
+    }
   }
-};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-
-    default:
-      return state;
-  }
-}
-
-export default reducer;
+  
+  export default reducer;
