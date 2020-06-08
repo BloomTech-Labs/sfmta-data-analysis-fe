83730aef8bf5921de8ffcf44f48ba2ecf2d74b62
Justin Russell <Justin@justins-macbook.lan>
2020-06-08 10:36:45 -0700

    another action

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -4,14 +4,27 @@ import axios from 'axios'
 
 export const SET_DATE = "SET_DATE";
 export const SET_TRANSIT_TYPE = "SET_TRANSIT_TYPE";
+export const GET_ROUTE = "GET_ROUTE";
+
 
 export const getType = () => dispatch => {
     axios.get("http://datadriventransit-env.eba-f6pyasyj.us-east-1.elasticbeanstalk.com/api/type")
         .then(res => {
-            console.log(res);
-            dispatch({ type: SET_TRANSIT_TYPE })
+            console.log(res.data);
+            dispatch({ type: SET_TRANSIT_TYPE, payload: res.data })
         })
         .catch(error => {
             console.log(error, "cant get data");
+        })
+}
+
+export const sendType = param => dispatch => {
+    axios.post("http://datadriventransit-env.eba-f6pyasyj.us-east-1.elasticbeanstalk.com/api/type", param)
+        .then(res => {
+            console.log(res);
+            dispatch({ type: GET_ROUTE })
+        })
+        .catch(error => {
+            console.log(error, "cant send data")
         })
 }
