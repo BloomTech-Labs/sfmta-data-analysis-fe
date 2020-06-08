44c537bcf86d6605189c8152d7a3c5985d4f322a
Justin Russell <Justin@justins-macbook.lan>
2020-06-08 10:03:59 -0700

    actions

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,2 +1,17 @@
 import axios from 'axios'
 
+// http://datadriventransit-env.eba-f6pyasyj.us-east-1.elasticbeanstalk.com/
+
+export const SET_DATE = "SET_DATE";
+export const SET_TRANSIT_TYPE = "SET_TRANSIT_TYPE";
+
+export const getType = () => dispatch => {
+    axios.get("http://datadriventransit-env.eba-f6pyasyj.us-east-1.elasticbeanstalk.com/api/type")
+        .then(res => {
+            console.log(res);
+            dispatch({ type: SET_TRANSIT_TYPE })
+        })
+        .catch(error => {
+            console.log(error, "cant get data");
+        })
+}
