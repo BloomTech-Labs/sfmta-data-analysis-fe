52a3ec808afb7e5d70e432d5c2405fcb0ed39754
Justin Russell <Justin@justins-macbook.lan>
2020-06-08 10:54:16 -0700

    actions

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 export const SET_DATE = "SET_DATE";
 export const SET_TRANSIT_TYPE = "SET_TRANSIT_TYPE";
 export const GET_ROUTE = "GET_ROUTE";
+export const GET_COORDINATES = "GET_COORDINATES";
 
 
 export const getType = () => dispatch => {
@@ -19,12 +20,23 @@ export const getType = () => dispatch => {
 }
 
 export const sendType = param => dispatch => {
-    axios.post("http://datadriventransit-env.eba-f6pyasyj.us-east-1.elasticbeanstalk.com/api/type", param)
+    axios.post("http://datadriventransit-env.eba-f6pyasyj.us-east-1.elasticbeanstalk.com/api/route", param)
         .then(res => {
             console.log(res);
             dispatch({ type: GET_ROUTE })
         })
         .catch(error => {
             console.log(error, "cant send data")
+        })
+}
+
+export const getCoordinates = id => dispatch => {
+    axios.get(`http://datadriventransit-env.eba-f6pyasyj.us-east-1.elasticbeanstalk.com/api/route/${id}`)
+        .then(res => {
+            console.log(res);
+            dispatch({ type: GET_COORDINATES })
+        })
+        .catch(error => {
+            console.log(error, "couldnt get coord")
         })
 }
