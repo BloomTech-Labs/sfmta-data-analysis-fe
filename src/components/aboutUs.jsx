e613b5ad2662effdc5fe3d10ed375d0c8bd60617
Kevin <knphillips01@gmail.com>
2020-06-08 19:41:43 -0500

    removed uncontrolled alert - click orange marker to learn more about team

--- src/components/aboutUs.jsx
+++ src/components/aboutUs.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import AboutUsMap from './aboutUsMap';
-import { UncontrolledAlert } from 'reactstrap';
+
 import {StyledH2} from '../style'
 
 const AboutUs = props => {
     return (
         <div className="about-us">
             <StyledH2>Our Team</StyledH2>
-            <UncontrolledAlert color='warning'>
-                Click an orange marker to learn more about the team!
-            </UncontrolledAlert>
+            
             <AboutUsMap/>
         </div>
     )
