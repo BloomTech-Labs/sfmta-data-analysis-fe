49e0d249a3675b982330f88aa84ca4ab975624af
Justin Kuenzinger <jkuenzinger228@gmail.com>
2020-06-15 10:03:54 -0700

    adding testing for rendering components

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { RightPanel } from '../right-panel/rightPanel';
-import LeftPanel from '../Left-panel/leftPanel';
+import LeftPanel from '../Left-panel/LeftPanel';
 import CentralPanel from '../central-panel/centralPanel';
 import DashboardNav from './DashboardNav';
 
