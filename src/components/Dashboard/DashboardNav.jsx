2b8b5f28dc12e23b210dd69e994a8b5d4e44faa6
Justin Russell <Justin@justins-macbook.lan>
2020-06-15 08:23:20 -0700

    cleaning working tree

--- src/components/Dashboard/DashboardNav.jsx
+++ src/components/Dashboard/DashboardNav.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, useParams } from "react-router-dom";
 import { NavItem } from 'reactstrap';
-import { getRoutereport } from '../../actions/index.jsx';
+import { getReport } from '../../actions/index.jsx';
 import { connect } from 'react-redux';
 
 
@@ -14,7 +14,7 @@ const DashboardNav = (props) => {
     const { id } = useParams()
 
     const handleSubmit = () => {
-        props.getRoutereport({ route_type: id || 'All' })
+        props.getReport({ route_type: id || 'All' })
     }
 
     useEffect(() => {
@@ -58,4 +58,4 @@ const DashboardNav = (props) => {
     )
 }
 
-export default connect(null, { getRoutereport })(DashboardNav);
+export default connect(null, { getReport })(DashboardNav);
