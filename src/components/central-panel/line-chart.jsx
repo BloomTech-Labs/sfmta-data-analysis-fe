580bbda8bf41e077e31ce2f2b1a18aaff909a745
Justin Kuenzinger <jkuenzinger228@gmail.com>
2020-06-23 08:34:38 -0700

    added line graph styling

--- src/components/central-panel/line-chart.jsx
+++ src/components/central-panel/line-chart.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2'
 import { connect } from "react-redux"
-
 export function LineChart(props) {
- return (
+  return (
     <div className="line-chart">
       <Line
-    
         data={{
           labels: props.line_chart ? props.line_chart.times : null,
           datasets: [
@@ -22,12 +20,10 @@ export function LineChart(props) {
               label: '# bunches',
               backgroundColor: ["#FF6D37", "#232323"],
               borderColor: '#FF4500',
-          
               fill: false
             }
           ]
         }}
-
         options={{
           responsive: true,
           aspectRatio: 1,
@@ -39,15 +35,11 @@ export function LineChart(props) {
                   lineWidth: 3,
                   zeroLineWidth: 3,
                   zeroLineColor: "#40FFCE",
-                  fontColor: "#40FFCE",
-                  scaleLabel: {
-                    fontColor: "#40FFCE"
-                  }
                 },
               ticks: {
                 max: props.line_chart ? getMaxYValue(props.line_chart.gaps, props.line_chart.bunches) : 0,
                 stepSize: 50,
-                
+                fontColor: "#40FFCE"
               }
             }],
             xAxes: [{
@@ -56,34 +48,25 @@ export function LineChart(props) {
                 lineWidth: 3,
                 zeroLineWidth: 3,
                 zeroLineColor: "#40FFCE",
-                fontColor: "#40FFCE",
-                scaleLabel: {
-                  fontColor: "#40FFCE"
-                }
               },
             ticks: {
               max: props.line_chart ? getMaxYValue(props.line_chart.gaps, props.line_chart.bunches) : 0,
               stepSize: 50,
-                   
+              fontColor: "#40FFCE"
             }
           }],
-            
           }
-
         }}
       />
     </div>
   )
 };
-
 function getMaxYValue(gaps, bunches) {
   return Math.max(...gaps, ...bunches);
 }
-
 const mapStateToProps = state => {
   return {
     line_chart: state.report.line_chart
   }
 }
-
 export default connect(mapStateToProps, {})(LineChart);
